d19642f9b9ddfe79294584640795f2e7ea52479a
MarinaPerezR <marina.perezrobles@gmail.com>
2024-12-10 18:42:46 +0000

    Added the footer links

--- src/App.js
+++ src/App.js
@@ -4,7 +4,12 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather App
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a href='https://github.com/MarinaPerezR' rel="noreferrer" target='_blank'>Marina P.R.</a> and is open-sourced on{" "}
+        <a href='https://github.com/MarinaPerezR/project-react-weather-app' rel="noreferrer" target='_blank'>GitHub</a>
+      </footer> 
     </div>
   );
 }
