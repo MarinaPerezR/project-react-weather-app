ebd26147a26762509be5b045ca53ad50ae3b5c7b
MarinaPerezR <marina.perezrobles@gmail.com>
2024-12-10 19:08:53 +0000

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-
+import React from "react";
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a href='https://github.com/MarinaPerezR' rel="noreferrer" target='_blank'>Marina P.R.</a> and is open-sourced on{" "}
-        <a href='https://github.com/MarinaPerezR/project-react-weather-app' rel="noreferrer" target='_blank'>GitHub</a>
-      </footer> 
+      <div className="container">
+       <h1>Weather App</h1>
+       <footer>
+          This project was coded by{" "}
+          <a href='https://github.com/MarinaPerezR' rel="noreferrer" target='_blank'>Marina P.R.</a> and is open-sourced on{" "}
+          <a href='https://github.com/MarinaPerezR/project-react-weather-app' rel="noreferrer" target='_blank'>GitHub</a>
+       </footer> 
+      </div>
     </div>
   );
 }
